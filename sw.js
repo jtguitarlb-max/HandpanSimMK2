3c362eb8fc4d1075decc9ceae81bad59c10432f8
jtguitarlb-max <jtguitarlb@gmail.com>
2026-09-06 04:32:50 +0100

    Add files via upload
    Layout fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='handpan-mk2-tunings-v8';
+const CACHE='handpan-mk2-tunings-v9-fix';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./metal-texture.png','./ambience-beach.mp3','./ambience-forest.mp3','./ambience-stream.mp3','./ambience-rain.mp3'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});

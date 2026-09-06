8ef30a8b650232a6baccf46be650f911ac456555
jtguitarlb-max <jtguitarlb@gmail.com>
2026-09-06 17:34:41 +0100

    Add files via upload
    50% all v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocket-handpan-defaults-v24';
+const CACHE='pocket-handpan-defaults-v25';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./metal-texture.png','./pocket-handpan-logo.png','./ambience-beach.mp3','./ambience-forest.mp3','./ambience-stream.mp3','./ambience-rain.mp3'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});

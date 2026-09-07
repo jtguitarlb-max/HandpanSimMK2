f0f757830c437a9d4d66f80cc5ec89d63e472e79
jtguitarlb-max <jtguitarlb@gmail.com>
2026-09-07 07:07:22 +0100

    Add files via upload
    Hide ambient volume in menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocket-handpan-egg-shaker-v28';
+const CACHE='pocket-handpan-ambience-settings-v29';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./metal-texture.png','./pocket-handpan-logo.png','./ambience-beach.mp3','./ambience-forest.mp3','./ambience-stream.mp3','./ambience-rain.mp3','./egg-shaker.wav'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
